28fadf6d76bbd60d89dfbf065803a79ebc79d82d
cepheix <greenerenator@gmail.com>
2015-06-11 15:10:53 +0200

    fixed specs for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,10 @@ class ProductsController < ApplicationController
   expose(:review) { Review.new }
   expose_decorated(:reviews, ancestor: :product)
 
+  before_action :signed_in_user, only: [:create, :update, :destroy]
+
   def index
+    @products = Product.all
   end
 
   def show
@@ -15,36 +18,52 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    if current_user != product.user
+      redirect_to category_product_url(category, product)
+      flash[:error] = 'You are not allowed to edit this product.'
+    end
   end
 
   def create
     self.product = Product.new(product_params)
-
     if product.save
       category.products << product
-      redirect_to category_product_url(category, product), notice: 'Product was successfully created.'
+      redirect_to category_product_url(category, product),
+                  notice: 'Product was successfully created.'
     else
       render action: 'new'
     end
   end
 
   def update
-    if self.product.update(product_params)
-      redirect_to category_product_url(category, product), notice: 'Product was successfully updated.'
+    if current_user == product.user
+      if self.product.update(product_params)
+        redirect_to category_product_url(category, product),
+                    notice: 'Product was successfully updated.'
+      else
+        render action: 'edit'
+      end
     else
-      render action: 'edit'
+      redirect_to category_product_url(category, product)
+      flash[:error] = 'You are not allowed to edit this product.'
     end
   end
 
   # DELETE /products/1
   def destroy
     product.destroy
-    redirect_to category_url(product.category), notice: 'Product was successfully destroyed.'
+    redirect_to category_url(product.category),
+                notice: 'Product was successfully destroyed.'
   end
 
   private
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :category_id)
+  end
+
+  def signed_in_user
+    redirect_to new_user_session_path,
+                notice: "You are not logged in" unless user_signed_in?
   end
 end
